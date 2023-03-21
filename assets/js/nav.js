dc3dc9f176ccf94112bfcb9b913ff3732a5bf05e
josvelema <volumensolutions@gmail.com>
2023-03-21 21:24:32 +0100

    color pallete final , general styling, new content , new logo

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -33,7 +33,7 @@ const navObserver = new IntersectionObserver((watchEntry) => {
   !watchEntry[0].isIntersecting ? nav.classList.add('active') : nav.classList.remove('active');
 }, {threshold: 0.85});
 
-navObserver.observe(document.querySelector('.profile-picture'));
+navObserver.observe(document.querySelector('#changeNavColor'));
 
 const fadeUpObserver = new IntersectionObserver((elsToWatch) => {
   elsToWatch.forEach((el) => {
@@ -42,7 +42,7 @@ const fadeUpObserver = new IntersectionObserver((elsToWatch) => {
       fadeUpObserver.unobserve(el.target);
     }
   });
-}, {threshold: 0.10});
+}, {threshold: 0.9});
 
 document.querySelectorAll('.fade-up').forEach((item) => {
   fadeUpObserver.observe(item);
